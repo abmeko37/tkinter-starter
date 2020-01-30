bcc42c4a6ca8da89cf5e1ce08bf68f1030b56de2
Abrahim <60404056+abmeko37@users.noreply.github.com>
2020-01-30 14:33:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # A starter program for Python with Tkinter
 
 from tkinter import * # import Tkinter library
+from tkinter.ttk import *
 from tkinter import Menu
+from tkinter.ttk import Progressbar
+from tkinter import ttk
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
@@ -14,11 +17,37 @@ chk_state = BooleanVar()
 chk_state.set(True)
 chk = Checkbutton(window, text='Choose', var=chk_state)
 chk.grid(column=1, row=3)
-btn = Button(window, text="Click Me")
-btn.grid(column=0, row=2)
 menu = Menu(window)
 new_item = Menu(menu)
 new_item.add_command(label='New')
 menu.add_cascade(label='File', menu=new_item)
 window.config(menu=menu)
+style = ttk.Style()
+style.theme_use('default')
+style.configure("black.Horizontal.TProgressbar", background='yellow')
+bar = Progressbar(window, length=100, style='black.Horizontal.TProgressbar')
+bar.grid(column=0, row=0)
+spin = Spinbox(window, from_=-100, to=100, width=10)
+spin.grid(column=3,row=0)
+progress = Progressbar(window, orient = HORIZONTAL, 
+              length = 100, mode = 'determinate') 
+def bar(): 
+    import time 
+    progress['value'] = 20
+    window.update_idletasks() 
+    time.sleep(1) 
+
+    progress['value'] = 30
+    window.update_idletasks() 
+    time.sleep(1) 
+    progress['value'] = 60
+    window.update_idletasks() 
+    time.sleep(1) 
+    progress['value'] = 80
+    window.update_idletasks() 
+    time.sleep(1) 
+    progress['value'] = 100
+
+progress.pack(pady=10)
+Button(window, text="Click Me" command=bar).pack(pady=10)
 window.mainloop()     # Keep the window open
